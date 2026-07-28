25b539ccb5c151ebeff7fea905f1ac7ee20a3f5d
Vashchenko Lev <l.vashchenko@neuromed.tech>
2026-07-28 10:09:51 +0300

    feat(web): scaffold nextjs app with fsd skeleton

--- apps/web/src/app/(public)/page.tsx
+++ apps/web/src/app/(public)/page.tsx
@@ -1,6 +1,6 @@
 export default function LandingPage() {
   return (
-    <main className="mx-auto flex justify-center flex-col g-4">
+    <main className="mx-auto flex justify-center flex-col gap-4">
       <h1>Main</h1>
       <h2>Dashboard</h2>
     </main>
